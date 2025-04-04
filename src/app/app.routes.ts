63c7cdc7a1edb9fe9a44507f765ed1aabe85715b
dile <duyle131103@gmail.com>
2025-04-04 20:44:58 +0700

    Revert "Chore: refactor structure folder project"
    This reverts commit 7434da3610636e8618a91287c132c24a01b144f6.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
-import { ErrorPageComponent } from './shared/_components/error-page/error-page.component';
-import { LandingPageComponent } from './pages/landing-page/landing-page.component';
-import { HomeComponent } from './pages/landing-page/home/home.component';
-import { DiscoverComponent } from './pages/landing-page/discover/discover.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: LandingPageComponent,
-    children: [
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: 'home', component: HomeComponent },
-      { path: 'discover', component: DiscoverComponent}
-    ]
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent
   },
   {
     path: 'sign-in',
