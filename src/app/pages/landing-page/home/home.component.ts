b1c57eec87518cd00e9cea18cf2ef5f6f1f08569
dile <duyle131103@gmail.com>
2025-04-10 23:47:51 +0700

    feat: add style release section

--- src/app/pages/landing-page/home/home.component.ts
+++ src/app/pages/landing-page/home/home.component.ts
@@ -11,7 +11,8 @@ import { CarouselModule } from 'primeng/carousel';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
-  movies: any[] = [];
+  topRatedMovies: any[] = [];
+  nowPlayingMovies: any[] = [];
   genresMap: { [id: number]: string } = {};
 
   constructor(private moviesService: MoviesService) {}
@@ -24,13 +25,29 @@ export class HomeComponent implements OnInit {
       });
   
       // Sau khi genres có, mới lấy movies
-      this.moviesService.getMovies().subscribe((movieData: any) => {
-        this.movies = movieData.results.slice(0, 5);
+      this.moviesService.getTopRatedMovies().subscribe((data: any) => {
+        this.topRatedMovies = data.results.slice(0, 5);
+
+        this.topRatedMovies.forEach((movie, index) => {
+          this.moviesService.getMovieDetail(movie.id).subscribe((detail: any) => {
+            movie.formattedRuntime = this.formatRuntime(detail.runtime);
+          })
+        })
       });
+
+      this.moviesService.getNowPlayingMovies().subscribe((data: any) => {
+        this.nowPlayingMovies = data.results.slice(0, 10);
+      })
     });
   }
 
   getGenreNames(genreIds: number[]): string[] {
     return genreIds.map(id => this.genresMap[id]).filter(name => !!name);
+  }
+
+  formatRuntime(minutes: number): string {
+    const hours = Math.floor(minutes / 60);
+    const remainingMinutes = minutes % 60;
+    return `${hours}h${remainingMinutes}m`;
   }
 }
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -8,17 +8,27 @@ import { environment } from '../../environments/environment';
 })
 export class MoviesService {
   private genreUrl = `${environment.apiUrl}/genre/movie/list?language=en&api_key=${environment.apiKey}`;
-  private moviesUrl = `${environment.apiUrl}/movie/top_rated?language=en-US&page=1&api_key=${environment.apiKey}`;
-  movies: any[] = [];
+  private topRatedMoviesUrl = `${environment.apiUrl}/movie/top_rated?language=en-US&page=1&api_key=${environment.apiKey}`;
+  private nowPlayingMoviesUrl = `${environment.apiUrl}/movie/now_playing?language=en-US&page=1&api_key=${environment.apiKey}`;
+  
+  topRatedMovies: any[] = [];
   genres: any[] = [];
 
   constructor(private http: HttpClient) {}
 
-  getMovies() {
-    return this.http.get(this.moviesUrl)
+  getTopRatedMovies() {
+    return this.http.get(this.topRatedMoviesUrl)
   }
 
   getGenres() {
     return this.http.get(this.genreUrl);
+  }
+
+  getMovieDetail(id: number) {
+    return this.http.get(`${environment.apiUrl}/movie/${id}?language=en-US&api_key=${environment.apiKey}`)
+  }
+
+  getNowPlayingMovies() {
+    return this.http.get(this.nowPlayingMoviesUrl);
   }
 }
